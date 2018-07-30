79144f1fe6aa2c3713891cd32a11eef667334fe5
j- <j@skeoh.com>
2018-07-30 11:31:28 +1000

    Calculate discounts with total

--- checkout.js
+++ checkout.js
@@ -15,10 +15,23 @@ class Checkout {
 	}
 
 	total () {
+		return this.subtotal() - this.discounts();
+	}
+
+	subtotal () {
 		return this.items
 			.map((item) => prices.get(item))
 			.filter((item) => item !== undefined)
 			.reduce((total, price) => total + price, 0);
+	}
+
+	discounts () {
+		if (!this.pricingRules || !this.pricingRules.length) {
+			return 0;
+		}
+		return this.pricingRules
+			.map((rule) => rule(this))
+			.reduce((total, discount) => total + discount, 0);
 	}
 }
 
