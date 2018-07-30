c57c106b98810b3d627476aec8c6199634f8530e
j- <j@skeoh.com>
2018-07-30 11:31:37 +1000

    Implement Unilever discount logic

--- rules/unilever.js
+++ rules/unilever.js
@@ -1,1 +1,11 @@
-module.exports = [];
+const prices = require('../prices');
+
+module.exports = [
+	/** 3 for 2 deal on classic ads */
+	(co) => {
+		const numClassics = co.items.filter((item) => item === 'classic').length;
+		const numThrees = Math.floor(numClassics / 3);
+		const discount = numThrees * prices.get('classic');
+		return discount;
+	},
+];
